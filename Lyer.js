b79d6345a6530f0ebe78690b7c9f04913bbbd776
CHADISTIL <45186528+CHADISTIL@users.noreply.github.com>
2025-05-26 23:44:51 +0100

    Update Lyer.js

--- Lyer.js
+++ Lyer.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-            const video = document.getElementById('mainVideo');
+            const video = document.getElementById('video');
             const videoContainer = document.getElementById('videoContainer');
             const controlsContainer = document.getElementById('controlsContainer');
             const progressContainer = document.getElementById('progressContainer');
